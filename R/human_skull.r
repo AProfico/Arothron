7069c0a9b1d8a3d09a740065da3162266c0d3aaa
Arothron <antonio.profico@uniroma1.it>
2018-04-13 18:27:18 +0100

    Delete human_skull.r

--- R/human_skull.r
+++
@@ -1,8 +0,0 @@
-#' @title example dataset
-#' @description 3D mesh of a human skull
-#' @name human_skull
-#' @docType data
-#' @author Antonio Profico, Alessio Veneziano, Marina Melchionna, Pasquale Raia
-#' @keywords Arothron
-#' @usage data(human_skull)
-NULL

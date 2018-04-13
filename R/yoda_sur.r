030c144313ee0b59f6d9ab20525732cc4ab5bb23
Arothron <antonio.profico@uniroma1.it>
2018-04-13 18:35:41 +0100

    Delete yoda_sur.r

--- R/yoda_sur.r
+++
@@ -1,8 +0,0 @@
-#' @title example dataset
-#' @description Mesh of Yoda
-#' @name yoda_sur
-#' @docType data
-#' @author Antonio Profico, Alessio Veneziano, Marina Melchionna, Pasquale Raia
-#' @keywords Arothron
-#' @usage data(yoda_sur)
-NULL

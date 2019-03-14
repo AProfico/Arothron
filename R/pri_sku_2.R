f4373e346632b73e6ee1036f8aba5a570a4e3aec
Arothron <antonio.profico@uniroma1.it>
2019-03-14 13:29:17 +0100

    Create pri_sku_2.R

---
+++ R/pri_sku_2.R
@@ -0,0 +1,8 @@
+#' @title example dataset
+#' @description list of 3D meshes containing 5 primates' skulls
+#' @name pri_sku_2
+#' @docType data
+#' @author Antonio Profico, Alessio Veneziano, Costantino Buzi, Marina Melchionna, Pasquale Raia
+#' @keywords Arothron
+#' @usage data(pri_sku_2)
+NULL
